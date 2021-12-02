cf0fb7fa22868c4aa7684e311a16ac04c7e1ea7d
Ayush Katiyar <ayushkatiyar0000gmail.com>
2021-12-03 03:10:02 +0530

    updated the routes, post and compose functionality

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const lodash = require('lodash');          // this package to required to compare the url names 
+const { lowerCase } = require("lodash");
 const { response } = require("express");
 
 
@@ -9,30 +11,38 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
-
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
 let posts = [];
 
 app.get("/", function (request, response) {
-    response.render("home", { homeContent: homeStartingContent , posts: posts });
-})
+    response.render("home", { posts: posts });
+});
 
 app.get("/about", function (request, response) {
-    response.render("about", { aboutContent: aboutContent });
+    response.render("about");
 });
 
 app.get("/contact", function (request, response) {
-    response.render("contact", { contactContent: contactContent });
+    response.render("contact");
 });
 
 app.get("/compose", function (request, response) {
     response.render("compose");
 });
 
+app.get("/posts/:postName", function (req, res) {
+    let requestTitle = lodash.lowerCase(req.params.postName);
+
+    posts.forEach(element => {
+        let storedtitle = lodash.lowerCase(element.PostTitle);
+
+        if (storedtitle === requestTitle) {
+            res.render("post", {
+                title: element.PostTitle,
+                content: element.PostBody,
+            });
+        }
+    })
+})
 
 app.post("/compose", function (request, response) {
     let content = {
@@ -40,12 +50,14 @@ app.post("/compose", function (request, response) {
         PostBody: request.body.PostBody,
     }
     posts.push(content);
-    console.log(posts);
+    // console.log(posts);
 
-    response.redirect("/") ;
+    response.redirect("/");
 });
 
-
+app.post("/home" ,function(request,response){
+    response.redirect("/compose") ;
+})
 // listens on port 3000 or any other port 
 app.listen(process.env.PORT || 3000, function (request, response) {
     console.log("Server started at port 3000 .")
